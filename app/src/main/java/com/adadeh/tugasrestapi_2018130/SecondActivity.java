2b5922011deab6aa8419cd6888566a83bf6328e9
MichaelHendra <michaelhendranata01@gmail.com>
2022-11-28 20:12:16 +0700

    Tugas RestAPI Mobile Programing 2018130

---
+++ app/src/main/java/com/adadeh/tugasrestapi_2018130/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.adadeh.tugasrestapi_2018130;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
